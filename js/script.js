bb808d884bffcc7e34bb633234369b98dc65c504
Victor-ZA <79744538+Victor-ZA@users.noreply.github.com>
2021-12-04 13:19:10 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,42 +1,24 @@
-var body = document.getElementsByName('slides');
+import Swiper from 'swiper'
+import 'swiper/swiper.min.css';
 
-var itemsData = [
-    {
-        nameItem: 'Вахтёр',
-        procentAlc: '1%',
-        ccal: '35',
-        structure: 'Ржаной хлеб, Вода, Солод, Соль',
-        infoItem: 'Терпкий и мощный, вкус этого кваса взбодрит после тяжёлого дня и придаст сил для вечерних приключений!',
-        price: '150 P.',
-        img: '../img/bottle-left.jpg'
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
     },
-    {
-        nameItem: 'Романтик',
-        procentAlc: '0,5%',
-        ccal: '27',
-        structure: 'Ячменный хлеб, Вода, Солод, Апельсин',
-        infoItem: 'Лёгкий и освежающий напиток поможет сохранять отличное настроение в течение всего дня.',
-        price: '90 P.',
-        img:'../img/bottle-right.jpg'
-    }
-];
-
-var product = function(){
-    let productItem = document.createElement('div');
-
-}
-
-var slides = document.querySelectorAll('.slides .slide');
-var currentSlide = 0;
-//var slideInterval = setInterval(nextSlide,8000);
- 
-function nextSlide() {
-    let productImg = document.querySelector('.product_item_first img');
-    productImg.src = itemsData[currentSlide].img;
-    if (currentSlide > itemsData.length - 1) {
-        currentSlide = 0;
-    } else { 
-        currentSlide += 1;
-        console.log('Зашел');
-    }
-}
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
